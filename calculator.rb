a7edffa4fc635497c84e7bbe091c66c068a7fcc8
dallas <dallas-hill@hotmail.com>
2019-11-09 14:30:53 +1000

    added option to user

---
+++ calculator.rb
@@ -0,0 +1,19 @@
+loop do
+    puts "options"
+    puts "1. Sum"
+    puts "2. subtract"
+    puts "3. divide"
+    puts "4. multiply"
+    puts "5. square"
+    puts "quit"
+end
+
+# Requirements
+# print options
+# should be able to quit
+# must be able to take 2 values:
+# add
+# subtract
+# divide
+# multiply
+# square
